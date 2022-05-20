fdf4912e4e34ed66b9ff061ba35b2548f0f2dbc3
junny0527 <bonjun03@naver.com>
2022-05-20 17:20:36 +0900

    jdbc과제

---
+++ src/com/javaex/ex01/AuthorDelete.java
@@ -0,0 +1,67 @@
+package com.javaex.ex01;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class AuthorDelete {
+
+	public static void main(String[] args) {
+		// 0. import java.sql.*;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		// ResultSet rs = null;
+
+		try {
+
+			// 1. JDBC 드라이버 (Oracle) 로딩
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			// 2. Connection 얻어오기
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
+
+			// 3. SQL문 준비 / 바인딩 / 실행
+			// SQL문 준비
+			String query = "";
+			query += " DELETE FROM author ";
+			query += " WHERE author_id = ? "; // 양옆 큰따옴표와 내용 사이 띄어쓰기**
+			System.out.println(query);
+
+			// 바인딩
+			pstmt = conn.prepareStatement(query);
+			pstmt.setInt(1, 1);
+
+			// 실행
+			int count = pstmt.executeUpdate(); // 쿼리문 실행 --> 성공 갯수 리턴
+
+			// 4.결과처리
+			System.out.println(count + "건이 삭제되었습니다.");
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("error: 드라이버 로딩 실패 - " + e);
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+
+			// 5. 자원정리
+
+			try {
+				/*
+				 * if (rs != null) { rs.close(); }
+				 */
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				System.out.println("error:" + e);
+			}
+		}
+
+	}
+
+}
---
+++ src/com/javaex/ex01/BookDelete.java
@@ -0,0 +1,69 @@
+package com.javaex.ex01;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class BookDelete {
+
+	public static void main(String[] args) {
+		// 0. import java.sql.*;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+
+		try {
+
+			// 1. JDBC 드라이버 (Oracle) 로딩
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			// 2. Connection 얻어오기
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
+
+			// 3. SQL문 준비 / 바인딩 / 실행
+			// SQL문 준비
+			String query = "";
+			query += " update book ";
+			query += "  "; // 양옆 큰따옴표와 내용 사이 띄어쓰기**
+			query += "  ";
+			query += "  ";
+			System.out.println(query);
+
+			// 바인딩
+			pstmt = conn.prepareStatement(query); // 문자열 커리로 만들기
+			pstmt.setString(1, ""); // ?(물음표) 중 1번째 --> 순서 중요
+			pstmt.setString(2, ""); // ?(물음표) 중 2번째 --> 순서 중요
+			pstmt.setInt(0, 0);
+
+			// 실행
+			int count = pstmt.executeUpdate(); // 쿼리문 실행 --> 성공 갯수 리턴
+
+			// 4.결과처리
+			System.out.println(count + "건이 삭제되었습니다.");
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("error: 드라이버 로딩 실패 - " + e);
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+
+			// 5. 자원정리
+
+			try {
+				
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				System.out.println("error:" + e);
+			}
+		}
+
+	}
+
+}
